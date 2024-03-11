e4ae88b7050bfaf3bcbf8bc14984aaa2739edefd
vikas924 <obroy902@gmail.com>
2024-03-11 15:32:38 +0530

    db setup

---
+++ db/migrate/20240310154012_add_omniauth_columns_to_users.rb
@@ -0,0 +1,8 @@
+class AddOmniauthColumnsToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :provider, :string
+    add_column :users, :uid, :string
+
+    add_index :users, [:provider, :uid], unique: true
+  end
+end
