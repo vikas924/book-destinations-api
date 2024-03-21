62033005a2348ec8d65169a39c70799754893095
Obimbo07 <obimboausts@gmail.com>
2024-03-21 21:21:16 +0300

    destination controller action

--- app/controllers/api/v1/destinations_controller.rb
+++ app/controllers/api/v1/destinations_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::DestinationsController  < ApplicationController
-    before_action :authenticate_user!
-
+    before_action :authenticate_user! 
     def index
         @destinations = Destination.all
         render json: @destinations
@@ -12,4 +11,41 @@ class Api::V1::DestinationsController  < ApplicationController
         rescue ActiveRecord::RecordNotFound
             render json: { status: 404, message: "Destination not found" }, status: :not_found
     end
+
+    def create
+    @destination = Destination.new(destination_params)
+
+    # Handle image upload
+    if params[:image_url].present? && params[:image_url].respond_to?(:read)
+      # Read the uploaded file and convert it to base64
+      image_data = Base64.strict_encode64(params[:image_url].read)
+      # Assign the base64 encoded image data to the image_url attribute
+      @destination.image_url = "data:image/#{params[:image_url].content_type.split('/').last};base64,#{image_data}"
+    end
+
+    if @destination.save
+      render json: @destination, status: :created
+    else
+      render json: { errors: @destination.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+def destroy
+    @destination = Destination.find(params[:id])
+    @destination.reservations.destroy_all
+    if @destination.destroy
+      render json: { status: 200, message: "Successfully deleted." }
+    else
+      render json: { errors: @destination.errors.full_messages }, status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotFound
+    render json: { status: 404, message: "Destination not found" }, status: :not_found
+  end
+
+  private
+
+  def destination_params
+    params.permit(:name, :description, :image_url, :price_per_night, :price_per_meal)
+  end
+
 end
